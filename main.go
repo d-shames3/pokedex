08063975f3bd9251c5fb9babef8375c549a95861
d-shames3 <dshames3@gmail.com>
2025-03-31 10:46:24 -0400

    feat: adding repl loop to main func

--- main.go
+++ main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	newScanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		newScanner.Scan()
+		userInput := newScanner.Text()
+		userInputClean := cleanInput(userInput)
+		if len(userInputClean) == 0 {
+			fmt.Println("no user input, try again")
+		} else {
+			fmt.Printf("Your command was: %s\n", userInputClean[0])
+		}
+	}
+
 }
