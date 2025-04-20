0daccf803d246343d6f4d24ace670d0a39c46c1c
d-shames3 <dshames3@gmail.com>
2025-04-20 11:37:21 -0400

    feat: added map command

--- repl.go
+++ repl.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	"github.com/d-shames3/pokedex/internal/pokeapi"
 )
 
 func cleanInput(text string) []string {
@@ -12,13 +14,13 @@ func cleanInput(text string) []string {
 	return words
 }
 
-func commandExit() error {
+func commandExit(config *apiCallConfig) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp() error {
+func commandHelp(config *apiCallConfig) error {
 	fmt.Println("Welcome to the Pokedex!\nUsage:")
 	for _, cmd := range getCliCommands() {
 		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
@@ -26,10 +28,49 @@ func commandHelp() error {
 	return nil
 }
 
+func commandMap(config *apiCallConfig) error {
+	if config.Next == "" {
+		config.Next = "https://pokeapi.co/api/v2/location-area"
+	}
+	currentUrl := config.Next
+
+	res, err := pokeapi.CallPokeApi(config.Next)
+	if err != nil {
+		return err
+	}
+	data, err := pokeapi.UnmarshalPokeapiResponse(res)
+	if err != nil {
+		return err
+	}
+
+	config.Previous = currentUrl
+	if data.Next != "" {
+		config.Next = data.Next
+	} else {
+		config.Next = ""
+	}
+
+	results := data.Results
+	if len(results) == 0 {
+		fmt.Println("No results returned")
+		return nil
+	}
+
+	for _, location := range results {
+		fmt.Printf("%s\n", location.Name)
+	}
+	return nil
+}
+
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(*apiCallConfig) error
+}
+
+type apiCallConfig struct {
+	Next     string
+	Previous string
 }
 
 func getCliCommands() map[string]cliCommand {
@@ -43,6 +84,11 @@ func getCliCommands() map[string]cliCommand {
 			name:        "help",
 			description: "Displays a help message",
 			callback:    commandHelp,
+		},
+		"map": {
+			name:        "map",
+			description: "Fetches next 20 locations from the PokeApi",
+			callback:    commandMap,
 		},
 	}
 	return cliCommands
